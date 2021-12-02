1fb9f61ef5c91c2123b554ad156b9151b1dd4fee
Jae Kim <9028364@gmail.com>
2021-12-02 23:35:27 +0900

    Add Player Scrolling

--- src/components/SongItem.vue
+++ src/components/SongItem.vue
@@ -1,16 +1,6 @@
 <template>
   <li
-    class="
-      flex
-      justify-between
-      items-center
-      p-3
-      pl-6
-      cursor-pointer
-      transition
-      duration-300
-      hover:bg-gray-50
-    "
+    class="flex justify-between items-center p-3 pl-6 cursor-pointer transition duration-300 hover:bg-gray-50"
   >
     <div>
       <router-link
@@ -22,10 +12,16 @@
     </div>
 
     <div class="text-gray-600 text-lg">
-      <span class="comments">
-        <i class="fa fa-comments text-gray-600"></i>
-        {{ song.comment_count }}
-      </span>
+      <router-link
+        custom
+        :to="{ name: 'song', params: { id: song.docID }, hash: '#comments' }"
+        v-slot="{ navigate }"
+      >
+        <span class="comments" @click="navigate">
+          <i class="fa fa-comments text-gray-600"></i>
+          {{ song.comment_count }}
+        </span>
+      </router-link>
     </div>
   </li>
 </template>
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import { createStore } from 'vuex';
 import { Howl } from 'howler';
 import { auth, usersCollection } from '@/includes/firebase';
+import helper from '@/includes/helper';
 
 export default createStore({
   state: {
@@ -8,6 +9,9 @@ export default createStore({
     userLoggedIn: false,
     currentSong: {},
     sound: {},
+    seek: '00:00',
+    duration: '00:00',
+    playerProgress: '0%',
   },
   mutations: {
     toggleAuthModal(state) {
@@ -20,15 +24,28 @@ export default createStore({
       state.currentSong = payload;
       state.sound = new Howl({ src: [payload.url], html5: true });
     },
+    updatePosition(state) {
+      state.seek = helper.formatTime(state.sound.seek());
+      state.duration = helper.formatTime(state.sound.duration());
+      state.playerProgress = `${
+        (state.sound.seek() / state.sound.duration()) * 100
+      }%`;
+    },
   },
   getters: {
     // authModalShow: (state) => state.authModalShow,
+    playing: (state) => {
+      if (state.sound.playing) {
+        return state.sound.playing();
+      }
+      return false;
+    },
   },
   actions: {
     async register({ commit }, payload) {
       const userCred = await auth.createUserWithEmailAndPassword(
         payload.email,
-        payload.password
+        payload.password,
       );
 
       await usersCollection.doc(userCred.user.uid).set({
@@ -62,10 +79,48 @@ export default createStore({
       //   payload.router.push({ name: 'home' });
       // }
     },
-    async newSong({ commit, state }, payload) {
-      commit('newSong', payload);
+    async newSong({ commit, state, dispatch }, payload) {
+      if (state.sound instanceof Howl) {
+        state.sound.unload();
+      }
 
+      commit('newSong', payload);
       state.sound.play();
+      state.sound.on('play', () => {
+        requestAnimationFrame(() => {
+          dispatch('progress');
+        });
+      });
+    },
+    async toggleAudio({ state }) {
+      if (!state.sound.playing) {
+        return;
+      }
+      if (state.sound.playing()) {
+        state.sound.pause();
+      } else {
+        state.sound.play();
+      }
+    },
+    progress({ commit, state, dispatch }) {
+      commit('updatePosition');
+      if (state.sound.playing()) {
+        requestAnimationFrame(() => {
+          dispatch('progress');
+        });
+      }
+    },
+    updateSeek({ state, dispatch }, payload) {
+      if (!state.sound.playing) return;
+      const { x, width } = payload.currentTarget.getBoundingClientRect();
+      const clickX = payload.clientX - x;
+      const percentage = clickX / width;
+      const seconds = state.sound.duration() * percentage;
+
+      state.sound.seek(seconds);
+      state.sound.once('seek', () => {
+        dispatch('progress');
+      });
     },
   },
 });
